387c57862f792dbd9cc056f8267f00dc9d69db0e
dahunsi halima <dahunsiolajumoke18@gmail.com>
2022-10-17 04:48:09 -0700

    while loop added

--- task1.js
+++ task1.js
@@ -1,3 +1,5 @@
+
+// for loop
 let output = document.getElementById('output');
 
 
@@ -13,4 +15,23 @@ for (let x =0; x < score.length; x++){
             output.innerHTML += 'You are trapped because you failed <br>'
         }
     }
+
     } 
+
+    // while loop
+    let output2 = document.getElementById('output2');
+    let score2 = [60, 77, 53, 19, 59, 47, 23, 55]
+    let y=0;
+    while (y <= score2.length){
+        if((score2[y] - 5) > 50 ){
+            output2.innerHTML += 'You Passed <br>';
+        }else{
+            if (((score2[y] + (score2[y] - 5))/2) > 50){
+                output2.innerHTML += `You escaped my punishment because your original score was ${score2[y]} 
+                and my manipulated score is ${((score2[y] + (score2[y] - 5))/2)} <br>`;
+            }else{
+                output2.innerHTML += 'You are trapped because you failed <br>';
+            }
+        }
+        y++;
+    }
